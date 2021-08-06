ae598ec92feab31c94240d0d8529d8a5b36d6d89
Trenton Spears <techtrent@yahoo.com>
2021-08-06 18:08:53 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,78 +1,78 @@
+
+# Create your Game class logic in here
+
+# `Phrase` is imported into and used by `game.py`
 from phrase import Phrase
-import random
+# Used to take random phrases
+from random import choice as choose
 
 
 class Game:
-    def create_phrases(self):
-        phrases = [Phrase("Misfit Love"), Phrase("Battery Acid"), Phrase("God is in the Radio"), Phrase("Go With the Flow"), Phrase("Make it wit Chu")]
-        return phrases
-
-
-    def get_random_phrase(self):
-        return random.choice(self.phrases)
-
-
+    # `init` method that initializes all the requested attributes, which are all used later
     def __init__(self):
         self.missed = 0
-        self.phrases = self.create_phrases()
-        self.active_phrase = self.get_random_phrase()
+        self.phrases = [
+            ("Take me out to the ball game"),
+            ("We gonna rock down to electric avenue"),
+            ("Medium rare steak please"),
+            ("Please keep your hands and feet inside the vehicle at all times"),
+            ("We live on the most boring street in the country")
+        ]
+        self.active_phrase = None
         self.guesses = []
 
-
     def welcome(self):
-        print(" "*9+"_"*47+"""\n
-        === GET READY TO PLAY PH-PH-PH-PHRASE HUNTER ===
-        """+"_"*48, "\nGET READY TO CHOOSE YOUR LETTER AT THE PROMPT. YOU HAVE 5 GUESSES. LET'S GO !!!\n")
+        print("Welcome to Phrase Hunters! Good luck!\n")
 
+    # Sets the phrase that will be used in this particular game
+    def get_random_phrase(self):
+        return choose(self.phrases)
 
     def get_guess(self):
         while True:
-            user_guess = (input("Choose your letter: ")).lower()
-            if not user_guess.isalpha():
-                print("You may only choose alphabet characters, i.e. a-z, A-Z. Please try again.")
-            elif len(user_guess) != 1:
-                print("Sorry! You're only allowed one choice at a time. Please select a letter.")
+            # Guesses must be stored as uppercase, otherwise `phrase.display` will not work properly
+            guess = input("\nGuess a letter: ").upper()
+            # Logic for checking if the guess is a single letter
+            if guess.isalpha() and len(guess) == 1:
+                return guess
+                break
             else:
-                return user_guess
-            
-    def game_over(self):
-        print('Would you like to play again?')
-        answers = ['y', 'n']
-        answer = input('y/n: ').lower()
-        
-        while answer not in answers:
-            print("Please enter y or n...")
-            answer = input('y/n: ').lower()
+                # This will print indefinitely until the input function recieves an ideal input
+                print("\nPlease type a single letter!")
 
-        while answer == 'y':
-            game = Game()
-            game.start()
-            print('Would you like to play again?')
-            answer = input('y/n: ').lower()
-            while answer not in answers:
-                print("Please enter y or n...")
-                answer = input('y/n: ').lower()
-
-        print("\nWE HOPE YOU ENJOYED PLAYING PHRASE HUNTER! PLEASE COME BACK AND CHECK FOR NEW PHRASES AND CHALLENGES! ")
-
+    # A boolean is passed to this method that denotes whether the player has won or lost
+    def game_over(self, won: bool):
+        if won:
+            print("\nCongratulations! You guessed the phrase!")
+        else:
+            print("\nYou ran out of lives! Better luck next time!")
 
     def start(self):
-        self.welcome()
-        self.active_phrase.display(self.guesses)
-
-        while self.missed != 5:
-            print("You have currently used {} guesses.\n".format(self.missed))
-            user_guess = self.get_guess()
-            self.guesses.append(user_guess)
-            if self.active_phrase.check_letter(user_guess):
-                print("CORRECT!\n")
-                self.active_phrase.display(self.guesses)
-                if self.active_phrase.check_complete(self.guesses):
-                    print("CONGRATULATIONS! YOU UNCOVERED THE PHRASE!\n")
-                    break
-            if not self.active_phrase.check_letter(user_guess):
+        # Choose a phrase to use throughout the game
+        self.active_phrase = self.get_random_phrase()
+        # Pass that phrase to a new instance of the `Phrase` class
+        phrase = Phrase(self.active_phrase)
+        print("Welcome to Phrase Hunter! When prompted, guess a letter. 5 incorrect guesses and the game will end!\n")
+        while True:
+            # Always display the phrase first, updated with the user's guesses
+            phrase.display(self.guesses)
+            # If `phrase.hidden_phrase` has been updated and now matches the phrase chosen by the game, the player has won. Pass `True` to `game_over` and break the main loop
+            if phrase.check_complete():
+                self.game_over(True)
+                break
+            # If the user has guessed incorrectly more than 4 times, they have lost. Pass `False` to `game_over` and break the main loop
+            elif self.missed > 4:
+                self.game_over(False)
+                break
+            # If the player hasn't won or lost yet, we know their gameplay is still in progress
+            guess = self.get_guess()
+            # If the letter is present in the phrase (either uppercase or lowercase), add it to the list of guesses, thus ensuring the next call of `phrase.display` when the loop repeats will be up-to-date
+            if phrase.check_letter(guess):
+                self.guesses += guess
+            # Otherwise, the player has guessed incorrectly. Add a value of 1 to their `missed` guesses and print a warning message
+            else:
                 self.missed += 1
-                print("\nSORRY! THAT GUESS WAS INCORRECT. PLEASE TRY AGAIN.\n")
-
-        if self.missed == 5:
-            print("Sorry, you've used up all your choices... ")
+                print(
+                    f"\nYou have {5 - self.missed} out of 5 lives remaining!")
+            # Make some whitespace, simply for formatting purposes
+            print()
